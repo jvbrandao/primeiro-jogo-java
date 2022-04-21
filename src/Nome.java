3a905fcb68f6fe7fc83d576625883fa614f5c8db
João  victor brandão <72588230+jvbrandao@users.noreply.github.com>
2022-04-21 03:46:20 -0300

    atualizando nome

--- src/Nome.java
+++ src/Nome.java
@@ -14,11 +14,3 @@ public class Nome {
     }
 }
 
-//perguntar o nome da pessoa
-/*perguntar o nome da pessoa
- * perguntar se ela quer jogar -> 1 -sim 2 -não
- * se sim ocmeçar o jogo
- * o computador escolhe um numero
- * e eu vou colocando numeors de uma dez e verificar se é igual ao escolhido
- * se sim
- * jogador ganhou*/
